8796cb4683f2bdd604a36562bddea0f4b1ec4abd
JonathanDechelle <jonathan.dechelle@hotmail.com>
2016-03-28 00:36:18 -0400

    Create State machine for more clarity in futur code

---
+++ GameLibrairie/MyGameLibrairy/MyGameLibrairy/StateMachine.cs
@@ -0,0 +1,171 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyGameLibrairy
+{
+    public class StateMachine
+    {
+        private List<State> m_StateList;
+        private State m_CurrentState;
+        private State m_NextState;
+
+        public StateMachine()
+        {
+            m_StateList = new List<State>();
+        }
+
+        public void AddState(Enum aState, Status aStatus, Action aFunctionToCall)
+        {
+            State state = new State();
+            state.m_State = aState;
+            state.m_Status = aStatus;
+            state.m_FunctionToCall = aFunctionToCall;
+
+            m_StateList.Add(state);
+        }
+
+        public void SetState(Enum aState)
+        {
+            if (m_CurrentState == null)
+            {
+                m_CurrentState = new State();
+                m_CurrentState.m_State = aState;
+                m_CurrentState.m_Status = Status.OnEnter;
+                m_CurrentState.m_FunctionToCall = GetOnEnterFunction(m_CurrentState);
+
+                if (m_CurrentState.m_FunctionToCall != null)
+                {
+                    m_CurrentState.m_FunctionToCall();
+                }
+
+                m_NextState = GetStateCopy(m_CurrentState);
+            }
+
+            m_NextState.m_State = aState;
+        }
+
+        private State GetStateCopy(State aOriginalState)
+        {
+            State newState = new State();
+            newState.m_Status = aOriginalState.m_Status;
+            newState.m_FunctionToCall = aOriginalState.m_FunctionToCall;
+            return newState;
+        }
+
+        public void Update()
+        {
+            if (m_CurrentState.m_State != m_NextState.m_State)
+            {
+                SwitchNextState();
+            }
+
+            Action currentOnUpdateFunction = GetOnUpdateFunction(m_CurrentState);
+            if (currentOnUpdateFunction != null)
+            {
+                currentOnUpdateFunction();
+            }
+        }
+
+        private void SwitchNextState()
+        {
+            Action currentOnExitFunction = GetOnExitFunction(m_CurrentState);
+            if (currentOnExitFunction != null)
+            {
+                currentOnExitFunction();
+            }
+
+            m_CurrentState.m_State = m_NextState.m_State;
+
+            Action currentOnEnterFunction = GetOnEnterFunction(m_CurrentState);
+            if (currentOnEnterFunction != null)
+            {
+                currentOnEnterFunction();
+            }
+        }
+
+        private Dictionary<Action, Status> GetAllStatusFunction(State aState)
+        {
+            Dictionary<Action, Status> statusActionDicto = new Dictionary<Action, Status>();
+
+            for (int i = 0; i < m_StateList.Count; i++)
+            {
+                int stateToCheck = Convert.ToInt32(aState.m_State);
+                int stateInList = Convert.ToInt32(m_StateList[i].m_State);
+
+                if (stateToCheck == stateInList)
+                {
+                    statusActionDicto.Add(m_StateList[i].m_FunctionToCall, m_StateList[i].m_Status);
+                }
+            }
+
+            return statusActionDicto;
+        }
+
+        private Action GetOnEnterFunction(State aState)
+        {
+            Dictionary<Action, Status> statusActionDicto = GetAllStatusFunction(aState);
+
+            foreach (KeyValuePair<Action, Status> entry in statusActionDicto)
+            {
+                if (entry.Value == Status.OnEnter)
+                {
+                    return entry.Key;
+                }
+            }
+
+            return null;
+        }
+
+        private Action GetOnUpdateFunction(State aState)
+        {
+            Dictionary<Action, Status> statusActionDicto = GetAllStatusFunction(aState);
+
+            foreach (KeyValuePair<Action, Status> entry in statusActionDicto)
+            {
+                if (entry.Value == Status.OnUpdate)
+                {
+                    return entry.Key;
+                }
+            }
+
+            return null;
+        }
+
+        private Action GetOnExitFunction(State aState)
+        {
+            Dictionary<Action, Status> statusActionDicto = GetAllStatusFunction(aState);
+
+            foreach (KeyValuePair<Action, Status> entry in statusActionDicto)
+            {
+                if (entry.Value == Status.OnExit)
+                {
+                    return entry.Key;
+                }
+            }
+
+            return null;
+        }
+    }
+
+    public class State
+    {
+        public Enum m_State;
+        public Status m_Status;
+        public Action m_FunctionToCall;
+
+        public State()
+        {
+            /* Initialize */
+        }
+    }
+
+    public enum Status
+    {
+        OnEnter,
+        OnUpdate,
+        OnExit,
+    }
+}
--- TestCase/TestCase/TestCase/Game1.cs
+++ TestCase/TestCase/TestCase/Game1.cs
@@ -8,21 +8,75 @@ using Microsoft.Xna.Framework.GamerServices;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
+using MyGameLibrairy;
 
 namespace TestCase
 {
-    /// <summary>
-    /// This is the main type for your game
-    /// </summary>
+    public enum GameState
+    {
+        Intro,
+        MainMenu,
+        Options,
+        Level1,
+    }
+
     public class Game1 : Microsoft.Xna.Framework.Game
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
+        StateMachine m_StateMachine;
+        GameState m_CurrentGameState;
 
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
+
+            m_StateMachine = new StateMachine();
+            m_StateMachine.AddState(GameState.Intro, Status.OnEnter, OnEnterIntro);
+            m_StateMachine.AddState(GameState.Intro, Status.OnUpdate, OnUpdateIntro);
+            m_StateMachine.AddState(GameState.Intro, Status.OnExit, OnExitIntro);
+            m_StateMachine.AddState(GameState.MainMenu, Status.OnEnter, OnEnterMainMenu);
+            m_StateMachine.AddState(GameState.MainMenu, Status.OnUpdate, OnUpdateMainMenu);
+            m_StateMachine.AddState(GameState.MainMenu, Status.OnExit, OnExitMainMenu);
+
+            m_StateMachine.SetState(GameState.Intro);
+        }
+
+        public void OnEnterIntro()
+        {
+            int a = 3;
+        }
+
+        public void OnUpdateIntro()
+        {
+            int a = 2; 
+            KeyboardHelper.PlayerState = Keyboard.GetState();
+            if (KeyboardHelper.KeyPressed(Keys.A))
+            {
+                m_StateMachine.SetState(GameState.MainMenu);
+            }
+            KeyboardHelper.PlayerStateLast = Keyboard.GetState();
+        }
+
+        public void OnExitIntro()
+        {
+            int a = 3;
+        }
+
+        public void OnEnterMainMenu()
+        {
+            int a = 2;
+        }
+
+        public void OnUpdateMainMenu()
+        {
+            int a = 2;
+        }
+
+        public void OnExitMainMenu()
+        {
+            int a = 2;
         }
 
         protected override void Initialize()
@@ -43,6 +97,8 @@ namespace TestCase
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
                 this.Exit();
+
+            m_StateMachine.Update();
 
             base.Update(gameTime);
         }
