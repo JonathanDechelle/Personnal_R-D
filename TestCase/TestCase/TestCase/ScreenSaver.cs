265ea1f05f7e478402b1e16ab3294a001f783348
JonathanDechelle <jonathan.dechelle@hotmail.com>
2016-04-09 21:06:59 -0400

    added DeSerialize option

--- TestCase/TestCase/TestCase/ScreenSaver.cs
+++ TestCase/TestCase/TestCase/ScreenSaver.cs
@@ -29,6 +29,7 @@ namespace TestCase
         public static void SaveScreenContent(GameScreen aScreen)
         {
             SerializeScreen(aScreen, EditorManager.GetAllButtonInScreen(aScreen));
+            DeSerializeScreen(aScreen);
         }
 
         private static void SerializeScreen(GameScreen aScreen, List<EditableButton> aButtons)
@@ -37,11 +38,25 @@ namespace TestCase
             XmlSerializerNamespaces ns = new XmlSerializerNamespaces();
             ns.Add("", "");
 
-            using (var writer = new StreamWriter(m_ScreenRootDirectory + aScreen.GetType().Name.ToString() + ".xml"))
+            using (var writer = new StreamWriter(GetScreenPath(aScreen)))
             using (var xmlWriter = XmlWriter.Create(writer, new XmlWriterSettings { Indent = true, OmitXmlDeclaration = true }))
             {
                 serializer.Serialize(xmlWriter, aButtons, ns);
             }
+        }
+
+        private static void DeSerializeScreen(GameScreen aScreen)
+        {
+            var serializer = new XmlSerializer(typeof(List<EditableButton>));
+
+            List<EditableButton> screenButtons;
+            using (var reader = new StreamReader(GetScreenPath(aScreen)))
+                screenButtons = (List<EditableButton>)serializer.Deserialize(reader);
+        }
+
+        private static string GetScreenPath(GameScreen aScreen)
+        {
+            return m_ScreenRootDirectory + aScreen.GetType().Name.ToString() + ".xml";
         }
     }
 }
